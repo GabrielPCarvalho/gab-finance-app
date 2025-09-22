beff6e8430a3c4bd19d07b27a2a9c467564f8e58
gabrielpcarvalho <gabriel57_pereira@hotmail.com>
2025-09-22 00:32:57 -0300

    feat: config images

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,21 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const { withNativeWind } = require("nativewind/metro");
+const { withNativeWind } = require('nativewind/metro');
 
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: { assetExts, sourceExts },
+} = defaultConfig;
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  /* your config */
-});
+const svgConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
 
-module.exports = withNativeWind(config, { input: "./src/global.css" });
+const merged = mergeConfig(defaultConfig, svgConfig);
+
+module.exports = withNativeWind(merged, { input: './src/global.css' });
--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -3,6 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Private/home';
 import Settings from '../screens/Private/settings';
 
+import { House as HouseIcon } from 'lucide-react-native';
+import { Settings as SettingsIcon } from 'lucide-react-native';
+
 const TabRoutes = () => {
   const Tab = createBottomTabNavigator();
 
@@ -12,8 +15,20 @@ const TabRoutes = () => {
       initialRouteName="Home"
       screenOptions={{ headerShown: false }}
     >
-      <Tab.Screen name="Home" component={Home} options={{}} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: () => <HouseIcon />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: () => <SettingsIcon />,
+        }}
+      />
     </Tab.Navigator>
   );
 };
