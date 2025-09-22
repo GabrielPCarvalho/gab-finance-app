b99a4c62c9cf3252aa674f60830e2a6b2b12c70b
gabrielpcarvalho <gabriel57_pereira@hotmail.com>
2025-09-22 00:34:38 -0300

    feat: add colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,50 @@
 export const colors = {
-  primary: {
-    bp: '#55489D',
-    light: '#F2F0FF',
-  },
+  'primary-pure': '#2EE6C5',
+  'primary-light': '#',
+  'primary-medium': '#',
+  'primary-dark': '#',
+  'primary-ultra-light': '#',
+
+  'secondary-pure': '#1F2937',
+  'secondary-light': '#',
+  'secondary-medium': '#',
+  'secondary-medium-light': '#',
+  'secondary-dark': '#',
+
+  'tertiary-pure': '#4B5563',
+  'tertiary-light': '#',
+  'tertiary-medium': '#',
+  'tertiary-dark': '#',
+
+  'highlight-pure': '#F673AD',
+  'highlight-light': '#FDD9E9',
+  'highlight-medium': '#900945',
+  'highlight-dark': '#430420',
+
+  'low-pure': '#000000',
+  'low-light': '#A3A3A3',
+  'low-medium': '#666666',
+  'low-dark': '#292929',
+  'low-ulta-pure': '#00000051',
+
+  'high-pure': '#FFFFFF',
+  'high-light': '#F9FAFB',
+  'high-medium': '#E0E0E0',
+  'high-dark': '#CCCCCC',
+
+  'warning-pure': '#E1003C',
+  'warning-light': '#',
+  'warning-medium': '#BF0033',
+  'warning-dark': '#900027',
+
+  'success-pure': '#34A853',
+  'success-light': '#98E7AD',
+  'success-medium': '#31974C',
+  'success-dark': '#216B35',
+
+  'helper-pure': '#FF8A00',
+  'helper-light': '#FFE3C2',
+  'helper-extra-light': '#FFE3C230',
+  'helper-medium': '#CC6E00',
+  'helper-dark': '#7A4200',
 }
